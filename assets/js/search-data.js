400f0c3680e9430d050b8e1b9a7ae26c5ea66d60
cirodonaldesposito <96064103+cirodonaldesposito@users.noreply.github.com>
2026-04-29 13:22:07 +0000

    Deploying to gh-pages from @ cirodonaldesposito/cirodonaldesposito.github.io@95046fda34d8098b62bc4a0be111e5a153984c6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,9 +78,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-entrepreneurial-finance",
+          title: 'Entrepreneurial Finance',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "teachings-cases-in-entrepreneurship",
