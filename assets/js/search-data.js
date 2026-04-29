d189adf8ea6b883672d8f9229023e229062a6752
cirodonaldesposito <96064103+cirodonaldesposito@users.noreply.github.com>
2026-04-29 09:26:38 +0000

    Deploying to gh-pages from @ cirodonaldesposito/cirodonaldesposito.github.io@8a1f7b0a2bc53ce2c9092a5a5f3e7fb8fab78d59 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,6 +511,11 @@ ninja.data = [{
           description: "This course guides students through the end-to-end process of launching a new data-driven venture, from team formation and idea generation to securing funding and pitching to investors.",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/data-entrepreneurship-in-action-2025/";
+            },},{id: "teachings-digital-innovation-and-entrepreneurship",
+          title: 'Digital Innovation and Entrepreneurship',
+          description: "",
+          section: "Teachings",handler: () => {
+              window.location.href = "/teachings/digital-innovation-and-entrepreneurship-2022/";
             },},{id: "teachings-innovation-management",
           title: 'Innovation Management',
           description: "",
