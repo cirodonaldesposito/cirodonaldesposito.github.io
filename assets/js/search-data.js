1e86fa6f879f38ac732dd6e96cb96f17bfdddb66
cirodonaldesposito <96064103+cirodonaldesposito@users.noreply.github.com>
2026-04-29 07:39:13 +0000

    Deploying to gh-pages from @ cirodonaldesposito/cirodonaldesposito.github.io@367e766c1164061c10f446188cf4169b4adc9dfd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,21 +39,21 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
