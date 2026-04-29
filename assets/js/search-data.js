57162b4159e349d378d57fd506d79f71cea102bb
cirodonaldesposito <96064103+cirodonaldesposito@users.noreply.github.com>
2026-04-29 13:19:08 +0000

    Deploying to gh-pages from @ cirodonaldesposito/cirodonaldesposito.github.io@7ebb8b183a1656560e0d0a142421069e2dbb9d2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -73,9 +73,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-nlp-amp-llms",
+          title: 'NLP /&amp;amp; LLMs',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
