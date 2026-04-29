a56ecc99c7aa701a63d857284c5dffd28fad9ff8
cirodonaldesposito <96064103+cirodonaldesposito@users.noreply.github.com>
2026-04-29 13:24:10 +0000

    Deploying to gh-pages from @ cirodonaldesposito/cirodonaldesposito.github.io@6a79379f8427e8744ef51213b580857211f3275c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -88,6 +88,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-geopolitics-amp-innovation",
+          title: 'Geopolitics &amp;amp; Innovation',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{id: "teachings-cases-in-entrepreneurship",
           title: 'Cases in Entrepreneurship',
           description: "",
