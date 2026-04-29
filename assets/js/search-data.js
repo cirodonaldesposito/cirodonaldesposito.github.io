853ca18809dae96584c3b1c5a95bb2ccebd25de1
cirodonaldesposito <96064103+cirodonaldesposito@users.noreply.github.com>
2026-04-29 13:38:27 +0000

    Deploying to gh-pages from @ cirodonaldesposito/cirodonaldesposito.github.io@fa817f7418d13d191ede41d30c9687c8eeb02c86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,29 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/supervision/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-honoured-to-receive-the-best-reviewer-award-2025-from-industry-and-innovation-journal",
+        },{id: "news-honoured-to-receive-the-best-reviewer-award-2025-from-industry-and-innovation-journal",
           title: 'Honoured to receive the Best Reviewer Award 2025 from Industry and Innovation journal...',
           description: "",
           section: "News",},{id: "news-excited-to-share-that-my-paper-when-machines-meet-emotions-understanding-the-affective-side-of-ai-adoption-got-accepted-to-ipdmc-2026",
@@ -88,8 +66,8 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-geopolitics-amp-innovation",
-          title: 'Geopolitics &amp;amp; Innovation',
+            },},{id: "projects-ai-nlp-amp-llms",
+          title: 'AI, NLP &amp;amp; LLMs',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
