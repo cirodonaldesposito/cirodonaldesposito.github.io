28ad067c7defc1258d7d9c702d6d56a60fb93bb0
cirodonaldesposito <96064103+cirodonaldesposito@users.noreply.github.com>
2026-04-29 10:25:54 +0000

    Deploying to gh-pages from @ cirodonaldesposito/cirodonaldesposito.github.io@1447f9e4fca644c446ec4f848fa691b50032e587 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "",
@@ -36,13 +43,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "nav-people",
           title: "people",
