90da969cbd717aafd5a93fbcedd3ea9e7abb4e4c
cirodonaldesposito <96064103+cirodonaldesposito@users.noreply.github.com>
2026-08-18 08:43:04 +0000

    Deploying to gh-pages from @ cirodonaldesposito/cirodonaldesposito.github.io@b95a13bccf4d8b6fbb1b0bbbdb390424d6d1a3d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -81,13 +81,13 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-ai-nlp-amp-llms",
-          title: 'AI, NLP &amp;amp; LLMs',
+            },},{id: "projects-artificial-intelligence",
+          title: 'Artificial Intelligence',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-text-analysis",
-          title: 'Text analysis',
+            },},{id: "projects-natural-language-processing",
+          title: 'Natural Language Processing',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
